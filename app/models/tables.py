87569f348a2534cad3dfecaf808abed0f1c6ff27
kb5134 <frrssatto@gmail.com>
2022-08-05 21:39:07 -0300

    Banco de dados implementado e tabela de usuario criada

---
+++ app/models/tables.py
@@ -0,0 +1,20 @@
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
+
+
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+    def __repr__(self):
+        return "<User %r>" % self.username
+
