64f62a8f648c2484aa321abcd7e476fdf59b00d8
kb5134 <frrssatto@gmail.com>
2022-08-11 11:22:47 -0300

    Criação de produto configurada

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -76,5 +76,17 @@ def Rh():
 
 @app.route('/json')
 def json():
-    retorno = wcapi.get("products").json()
+    retorno = wcapi.get("products", params={"per_page": 20}).json()
     return  jsonify(retorno)
+
+@app.route('/criar')
+def criar():
+    produto = {
+    "sku": "Teste",
+    "name": "Teste Sistemas DIstribuidos",
+    "type": "simple",
+    "regular_price": "50.00",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris massa tellus, varius in tortor id, rhoncus pharetra arcu. Proin porta pharetra elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vestibulum urna in mi bibendum porta. Curabitur in nisi dignissim, venenatis odio id, interdum ante. Nam tristique mattis augue, non eleifend eros condimentum dapibus. Praesent facilisis eros a metus lobortis tristique. Aenean id fringilla enim. Pellentesque nec arcu non erat vestibulum condimentum a sit amet eros. ",
+    }
+    criar_produto = wcapi.post("products", produto).json()
+    return jsonify(criar_produto)
