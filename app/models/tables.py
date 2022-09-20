3e369ec28d9ffdbfc6367932bdc45edc82aca488
gMenez35 <jhowmenez@gmail.com>
2022-09-19 22:04:00 -0300

    19-09 Criacao de tabelas do banco

--- app/models/tables.py
+++ app/models/tables.py
@@ -36,3 +36,43 @@ class User(db.Model):
 
     def get_id(self):
         return str(self.id)
+
+#Estoque
+class Estoque(db.Model):
+    __tablename__ = "estoque"
+
+    sku = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    nome = db.Column(db.String(50))
+    quantidade = db.Column(db.Integer)
+    preco = db.Column(db.String(50))
+    descricao = db.Column(db.Text)
+
+    def __init__(self, sku, nome, quantidade, preco, descricao):
+        self.sku = sku
+        self.nome = nome
+        self.quantidade = quantidade
+        self.preco = preco
+        self.descricao = descricao
+
+    def __repr__(self):
+        return "<Estoque %r>" % self.nome
+
+#Financeiro
+class Financeiro(db.Model):
+    __tablename__ = "financeiro"
+
+    movimento = db.Column(db.Integer, primary_key=True) # Id da transação
+    receber = db.Column(db.Integer)
+    pagar = db.Column(db.Integer)
+    sku_Estoque = db.Column(db.Integer, db.ForeignKey('estoque.sku'))
+    #id_Fornecedor = db.Column(db.Integer, db.ForeignKey('fornecedor.sku'))
+
+    sku = db.relationship('Estoque', foreign_keys=sku_Estoque)
+    #id = db.relationship('Fornecedor', foreign_keys=id_Fornecedor)
+
+    def __init__(self, movimento, receber, pagar, sku_Estoque, id_Fornecedor):
+        self.movimento = movimento
+        self.receber = receber
+        self.pagar = pagar
+        self.sku_Estoque = sku_Estoque
+       #self.id_Fornecedor = id_Fornecedor
