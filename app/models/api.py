2b65ae8a0c24f14da34deb64605527d37763c4ed
Higor Amaro <amarohigor@gmail.com>
2022-09-29 00:00:52 -0300

    Alterações na tabela de lsitagem de estoque finalizadas e formatação pronta

--- app/models/api.py
+++ app/models/api.py
@@ -3,8 +3,8 @@ from woocommerce import API
 
 wcapi = API(
     url="http://localhost/wordpress", # Your store URL
-    consumer_key="ck_821ae5885990e933baea78120de03032954ccf39", # Api allef
-    consumer_secret="cs_8282a4332ac8067a070843ce9fa3c5e17fd2cc43", # Sempre trocar saporra
+    consumer_key="ck_26551b40eae612cf26c7ce14b705e070c9956675", # Api allef
+    consumer_secret="cs_a822e7ebf5a80fd1df8a82959876469d490ff317", # Sempre trocar saporra
     wp_api=True, # Enable the WP REST API integration
     version="wc/v3" # WooCommerce WP REST API version
 )
