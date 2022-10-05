ea66c939d7ed00243c54f34ffb8e5aa2f5a67e99
kb5134 <frrssatto@gmail.com>
2022-10-04 21:39:57 -0300

    Alteração ngrok para hospedar a aplicação

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_ngrok2 import run_with_ngrok
 
 app = Flask(__name__)
 app.config.from_object('config')
-
+#run_with_ngrok(app, auth_token='2FgufYFYVmF47Oh5O0EJ5mraGg9_4KUBtPyfTWMNTmQ3WmUiB')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db) # A instancia do migrate cuida das migrações pra isso ele recebe a aplicação e o banco
