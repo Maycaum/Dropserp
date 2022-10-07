bd5ff93078baa3b60234bab1c821ab838cfea35d
Higor Amaro <amarohigor@gmail.com>
2022-10-07 01:26:25 -0300

    Adicionando a função de CPJ

--- app/static/js/funcoesparaform.js
+++ app/static/js/funcoesparaform.js
@@ -3,3 +3,23 @@ function Apenasletras(input){
     // var regex = /[([aA-zZ]+)]/gi;
     input.value = input.value.replace(regex,"");
 }
+
+function mascara_cpf() {
+    var cpf = document.getElementById('cpf')
+    if(cpf.value.length == 3 || cpf.value.length == 7) { 
+        cpf.value+="."
+    } else if(cpf.value.length == 11) { 
+        cpf.value +="."
+    }
+}
+
+function mascara_cnpj() {
+    var cpf = document.getElementById('cpf')
+    if(cpf.value.length == 2 || cpf.value.length == 7) { 
+        cpf.value+="."
+    } else if(cpf.value.length == 11) { 
+        cpf.value +="."
+    }
+}
+
+
